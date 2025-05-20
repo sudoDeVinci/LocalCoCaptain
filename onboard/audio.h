af3ca5b177c9ab9687669a1fb5fd1851e62e0bba
Tadj cazaubon <tadj.d.cazaubon@gmail.com>
2025-05-20 07:23:55 +0200

    making a voice recorder to own the AV chuds

--- onboard/audio.h
+++ onboard/audio.h
@@ -4,38 +4,111 @@
 
 #include "io.h"
 
-
+/**
+ * Audio file class for WAV files.
+ */
 struct WAVFile {
-   /// The first 4 byte of a wav file should be the characters "RIFF" */
+    // WAV header fields
     char chunkID[4] = {'R', 'I', 'F', 'F'};
-    /// 36 + SubChunk2Size
-    uint32_t chunkSize = 36; // You Don't know this until you write your data but at a minimum it is 36 for an empty file
-    /// "should be characters "WAVE"
+    uint32_t chunkSize = 36; // Will be updated after writing data
     char format[4] = {'W', 'A', 'V', 'E'};
-    /// " This should be the letters "fmt ", note the space character
     char subChunk1ID[4] = {'f', 'm', 't', ' '};
-    ///: For PCM == 16, since audioFormat == uint16_t
     uint32_t subChunk1Size = 16;
-    ///: For PCM this is 1, other values indicate compression
-    uint16_t audioFormat = 1;
-    ///: Mono = 1, Stereo = 2, etc.
-    uint16_t numChannels = 1;
-    ///: Sample Rate of file
-    uint32_t sampleRate = 44100;
-    ///: SampleRate * NumChannels * BitsPerSample/8
-    uint32_t byteRate = 44100 * 2;
-    ///: The number of byte for one frame NumChannels * BitsPerSample/8
-    uint16_t blockAlign = 2;
-    ///: 8 bits = 8, 16 bits = 16
-    uint16_t bitsPerSample = 16;
-    ///: Contains the letters "data"
+    uint16_t audioFormat = 1; // PCM = 1
+    uint16_t numChannels = 1; // Mono = 1
+    uint32_t sampleRate = 16000; // Common for voice recording
+    uint32_t byteRate; // Calculated in constructor
+    uint16_t blockAlign; // Calculated in constructor
+    uint16_t bitsPerSample = 16; // 16-bit samples
     char subChunk2ID[4] = {'d', 'a', 't', 'a'};
-    ///: == NumSamples * NumChannels * BitsPerSample/8  i.e. number of byte in the data.
-    uint32_t subChunk2Size = 2116800; // You Don't know this until you write your data 
+    uint32_t subChunk2Size = 0; // Will be calculated based on actual data
 
-    int16_t* data = nullptr; // Pointer to the data buffer
-}
+    struct WaveForm {
+        int16_t* data = nullptr;
+        size_t size = 0; // Number of samples
+    } waveForm;
 
+    WAVFile() {
+        // Calculate dependent values
+        blockAlign = numChannels * bitsPerSample / 8;
+        byteRate = sampleRate * blockAlign;
+    }
+
+    // Method to set sample rate
+    void setSampleRate(uint32_t rate) {
+        sampleRate = rate;
+        byteRate = sampleRate * blockAlign;
+    }
+
+    // Method to allocate buffer for recording
+    bool allocateBuffer(size_t numSamples) {
+        if (waveForm.data != nullptr) {
+            free(waveForm.data);
+        }
+        waveForm.data = (int16_t*)malloc(numSamples * sizeof(int16_t));
+        if (waveForm.data == nullptr) {
+            return false;
+        }
+        waveForm.size = numSamples;
+        return true;
+    }
+
+    void write(fs::FS &filesystem, String filename) {
+        // Check if the waveForm data is not null
+        if (waveForm.data == nullptr || waveForm.size == 0) {
+            debugln("Waveform data is null or empty");
+            return;
+        }
+
+        // Calculate actual data size in bytes
+        subChunk2Size = waveForm.size * blockAlign;
+        chunkSize = 36 + subChunk2Size;
+
+        File wavFile = filesystem.open(filename, FILE_WRITE);
+        if (!wavFile) {
+            debugln("Failed to open file for writing");
+            return;
+        }
+
+        // Write the headers
+        wavFile.write(chunkID, 4);
+        wavFile.write((byte*)&chunkSize, 4);
+        wavFile.write(format, 4);
+        wavFile.write(subChunk1ID, 4);
+        wavFile.write((byte*)&subChunk1Size, 4);
+        wavFile.write((byte*)&audioFormat, 2);
+        wavFile.write((byte*)&numChannels, 2);
+        wavFile.write((byte*)&sampleRate, 4);
+        wavFile.write((byte*)&byteRate, 4);
+        wavFile.write((byte*)&blockAlign, 2);
+        wavFile.write((byte*)&bitsPerSample, 2);
+        wavFile.write(subChunk2ID, 4);
+        wavFile.write((byte*)&subChunk2Size, 4);
+
+        // Write the actual audio data
+        for (size_t i = 0; i < waveForm.size; i++) {
+            // Convert ADC value to WAV sample (-32768 to 32767)
+            int16_t sampleValue = map(waveForm.data[i], 0, 4095, -32768, 32767);
+            wavFile.write((byte*)&sampleValue, 2);
+        }
+        
+        wavFile.close();
+        debugln("WAV file written successfully");
+    }
+    
+    // Free the buffer when done
+    void freeBuffer() {
+        if (waveForm.data != nullptr) {
+            free(waveForm.data);
+            waveForm.data = nullptr;
+            waveForm.size = 0;
+        }
+    }
+    
+    ~WAVFile() {
+        freeBuffer();
+    }
+};
 
 #endif
 
--- onboard/onboard.ino
+++ onboard/onboard.ino
@@ -1,21 +1,122 @@
 #include <Arduino.h>
-#include "io.h"
+#include "audio.h"
 
-FS* fileSystem;
+// Pin definitions
+#define BUFFER_SIZE 16000 * 10  // 5 seconds at 16kHz
+#define SAMPLE_RATE 16000      // 16kHz sample rate
+#define ANALOG_PIN 9           // ADC input pin for MAX9814 microphone
+#define BUTTON_PIN 5           // Button pin (change as needed)
 
-#define BUFFER_SIZE 512
-#define SAMPLE_RATE 16000
-#define ANALOG_PIN 9 
+// Global file system pointer
+fs::FS* fileSystem = nullptr;
+
+// Button and recording state
+volatile bool buttonPressed = false;
+bool isRecording = false;
+unsigned long lastSampleTime = 0;
+unsigned long sampleInterval_us = 1000000 / SAMPLE_RATE; // microseconds between samples
+
+// Audio objects
+WAVFile wavFile;
+int recordingNumber = 0;
+
+// Interrupt handler for button press/release
+void IRAM_ATTR buttonISR() {
+  // Read button state (LOW when pressed if using pull-up)
+  buttonPressed = !digitalRead(BUTTON_PIN);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialize serial communication
   Serial.begin(115200);
   Serial.println("Hello, ESP32-S3!");
+  
+  // Configure button pin with internal pull-up and interrupt
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, CHANGE);
+  
+  // Configure ADC
+  pinMode(ANALOG_PIN, INPUT);
+  
+  // Initialize filesystem using the utility function
+  fileSystem = DetermineFileSystem();
+  if (!fileSystem) {
+    Serial.println("Failed to mount any filesystem");
+  }
+  
+  // Initialize WAV file
+  wavFile.setSampleRate(SAMPLE_RATE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  uint16_t freq = analogRead(ANALOG_PIN);
-  Serial.println(freq);
-  delay(25);
+  // Check if button state changed
+  if (buttonPressed && !isRecording) {
+    // Start recording
+    startRecording();
+    isRecording = true;
+  }
+  else if (!buttonPressed && isRecording) {
+    // Stop recording
+    stopRecording();
+    isRecording = false;
+  }
+  
+  // Handle recording
+  if (isRecording) {
+    unsigned long currentTime = micros();
+    if (currentTime - lastSampleTime >= sampleInterval_us) {
+      // Take a sample
+      if (wavFile.waveForm.size < BUFFER_SIZE) {
+        // Read from ADC and store in buffer
+        uint16_t sample = analogRead(ANALOG_PIN);
+        wavFile.waveForm.data[wavFile.waveForm.size++] = sample;
+      } else {
+        // If we've reached max buffer size, stop recording
+        debugln("Buffer full");
+        stopRecording();
+        isRecording = false;
+      }
+      lastSampleTime = currentTime;
+    }
+  }
+}
+
+void startRecording() {
+  debugln("Starting recording...");
+  
+  // Allocate buffer for recording
+  if (!wavFile.allocateBuffer(BUFFER_SIZE)) {
+    debugln("Failed to allocate memory for recording");
+    isRecording = false;
+    return;
+  }
+  
+  // Reset sample index and timestamp
+  wavFile.waveForm.size = 0;
+  lastSampleTime = micros();
+}
+
+void stopRecording() {
+  debugf("Recording stopped with %d samples\n", wavFile.waveForm.size);
+  
+  // Save recording if we have samples and a valid filesystem
+  if (fileSystem && wavFile.waveForm.size > 0) {
+    // Get current timestamp
+    time_t now;
+    time(&now);
+    tm* timeinfo = localtime(&now);
+    char* timestamp = formattime(timeinfo);
+    
+    // Create filename with timestamp
+    String filename = "/rec_" + String(recordingNumber++) + "_" + String(timestamp) + ".wav";
+    delete[] timestamp; // Free allocated memory
+    
+    debugf("Saving recording to %s\n", filename.c_str());
+    wavFile.write(*fileSystem, filename);
+  } else {
+    debugln("Could not save recording (no filesystem or empty buffer)");
+  }
+  
+  // Free the buffer
+  wavFile.freeBuffer();
 }
